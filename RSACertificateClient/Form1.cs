5756e9826fd9009e0b706920e5856c6caf76a171
Mantas Trojanovskis <mantastrojak@gmail.com>
2024-05-01 00:45:04 +0300

    Add project files.

---
+++ RSACertificateClient/Form1.cs
@@ -0,0 +1,10 @@
+namespace RSACertificateClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RSACertificateServer/Form1.cs
@@ -0,0 +1,10 @@
+namespace RSACertificate_Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
